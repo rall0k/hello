c3bbea52335c7d59535660d3314143570197b3fd
Lukáš Kollár <lukas.kollar@gmail.com>
2024-10-25 13:20:20 +0200

    hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
-  hub: {},
+  hub: {
+	database: true
+  },
 
   // https://eslint.nuxt.com
   eslint: {
